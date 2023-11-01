502bfde99e3f59ae12eba14798e6008ade06af9b
Amirhossein Dabaghmanesh <58168246+artinalbert@users.noreply.github.com>
2023-11-01 16:39:41 +0000

    solved

--- 08ex.c
+++ 08ex.c
@@ -15,6 +15,10 @@ Aufgabe 1a:
 Lesen Sie das Headerfile `turtlecanvas.h`. Diese Funktion soll die Turtle `d` Schritte vorwärts machen lassen.
 */
 void turtle_advance_by(TurtleCanvas *c, uint32_t d) {
+    for (uint32_t i = 0; i < d; i++)
+    {
+        turtle_advance(c);
+    }
     return;
 }
 
@@ -24,6 +28,25 @@ Füllen Sie die Turtlecanvas mit horizontalen, abwechselnd schwarzen und weißen
 schwarz gefärbt werden). Die Turtle ist anfangs an Position (0, 0), ist nach rechts orientiert, und zeichnet schwarz.
 */
 void turtle_stripes(TurtleCanvas *c) {
+    for (int y = 0; y < turtle_canvas_height(c); y++)
+    {
+        turtle_advance_by(c, turtle_canvas_width(c));
+        if(y%2==0)
+        {
+            turtle_toggle_color(c);
+            turtle_rotate_left(c);
+            turtle_advance(c);
+            turtle_rotate_left(c);
+        }
+        else
+        {
+            turtle_rotate_right(c);
+            turtle_advance(c);
+            turtle_rotate_right(c);
+            turtle_toggle_color(c);
+        }
+    }
+    
     return;
 }
 
@@ -39,7 +62,19 @@ Aufgabe 2a:
 Geben Sie einen Pointer auf das erste Vorkommen der größten Zahl im Eingabearray zurück.
 */
 uint16_t *find_maximal_number(uint16_t numbers[], size_t numbers_len) {
-    return numbers;
+    size_t max_pos = -1;
+    for (size_t i = 0; i < numbers_len; i++)
+    {
+        if(max_pos == -1 || numbers[max_pos] < numbers[i])
+        {
+            max_pos = i;
+        }
+    }
+    if(max_pos == -1)
+    {
+        return numbers;
+    }    
+    return &numbers[max_pos];
 }
 
 /*
@@ -47,7 +82,7 @@ Aufgabe 2b:
 Geben Sie (einen Pointer auf) das Teilarray zurück, welches ab dem ersten Vorkommen der größten Zahl im Eingabearray beginnt.
 */
 uint16_t *find_subarray_starting_at_maximal_number(uint16_t numbers[], size_t numbers_len) {
-    return numbers;
+    return find_maximal_number(numbers, numbers_len);
 }
 
 /*
@@ -56,7 +91,25 @@ Geben Sie die größtmögliche Distanz zwischen zwei Zahlenwerten aus dem Array 
 Beispiel: Im Array {1, 3, 7, 4} ist die größte Distanz die zwischen 1 und 7, und beträgt damit `6`.
 */
 uint16_t find_maximum_distance(uint16_t numbers[], size_t numbers_len) {
-    return 0;
+    if(numbers_len < 1)
+    {
+        return 0;
+    }
+
+    uint16_t max = 0;
+    uint16_t min = -1; // Underflow -> Max Value
+    for (size_t i = 0; i < numbers_len; i++)
+    {
+        if(max < numbers[i])
+        {
+            max = numbers[i];
+        }
+        if(min > numbers[i])
+        {
+            min = numbers[i];
+        }
+    }
+    return max - min;
 }
 
 /*
@@ -65,7 +118,19 @@ Geben Sie die kleinstmögliche Distanz zwischen zwei Zahlenwerten aus dem Array 
 Beispiel: Im Array {1, 3, 7, 4} ist die kleinste Distanz die zwischen 3 und 4, und beträgt damit `1`.
 */
 uint16_t find_minimum_distance(uint16_t numbers[], size_t numbers_len) {
-    return 0;
+    uint16_t distance = -1; // Underflow -> Max Value
+    for (size_t i = 0; i < numbers_len; i++)
+    {
+        for (size_t j = 0; j < numbers_len; j++)
+        {
+            if(i != j && distance > abs(numbers[i] - numbers[j]))
+            {
+                distance = abs(numbers[i] - numbers[j]);
+            }
+        }
+        
+    }
+    return distance;
 }
 
 /*
@@ -75,6 +140,11 @@ Hinweis: Wir starten bei `1`. Sollte numbers_len also `5` sein, sind die ersten 
 einschließlich die von 5 gemeint: 1, 4, 9, 16, 25.
 */
 void square_ascending(uint16_t numbers[], size_t numbers_len) {
+    for (size_t i = 1; i <= numbers_len; i++)
+    {
+        numbers[i-1] = i * i;
+    }
+    
     return;
 }
 
@@ -84,5 +154,23 @@ Füllen Sie das Array `out` mit den aufsteigend sortierten Zahlen aus dem `in` A
 Beispiel: Ist `in` {1, 4, 3, 7, 4}, so soll `out` am Ende {1, 3, 4, 4, 7} sein.
 */
 void sort_ascending(uint16_t in[], uint16_t out[], size_t len) {
+    for (size_t i = 0; i < len; i++)
+    {
+        out[i] = in[i];
+    }
+
+    for (size_t i = 0; i < len; i++)
+    {
+        size_t smallest_pos = i;
+        for (size_t j = i; j < len; j++)
+        {
+            if(out[j] < out[smallest_pos]){
+                smallest_pos = j;
+            }
+        }
+        uint16_t tmp = out[i];
+        out[i] = out[smallest_pos];
+        out[smallest_pos] = tmp;
+    }   
     return;
 }
