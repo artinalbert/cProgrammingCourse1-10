6a934749897cb5e195e9af01e2da9846cd8f5cf9
Amirhossein Dabaghmanesh <58168246+artinalbert@users.noreply.github.com>
2023-11-02 14:50:07 +0000

    Initial task 4

---
+++ 04ex_helpers.h
@@ -0,0 +1,16 @@
+#ifndef HEXHELPERS_H
+#define HEXHELPERS_H
+
+#include "04_canvas.h"
+
+/*
+Berechne die Distanz (abgerundet) zwischen dem Punkt `(x0, y0)` und dem Punkt `(x1, y1)`.
+*/
+int distance(int x0, int y0, int x1, int y1);
+
+/*
+Berechne die Quadratwurzel (abgerundet) von `n`.
+*/
+int squareroot(int n);
+
+#endif
